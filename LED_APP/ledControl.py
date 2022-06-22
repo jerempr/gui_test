909dd98f9abd185ee609ea59ef93218af2548d4b
Jérémie Probst <jprobst@operametrix.fr>
2022-06-22 11:54:57 +0000

    changes to match reterminal version of QT and organisation for easier use in a recipe

--- LED_APP/ledControl.py
+++ LED_APP/ledControl.py
@@ -1,38 +1,44 @@
 # import libraries
 import sys
 import os
-from PySide2.QtCore import *
+
+if 'PyQt5' in sys.modules:
+	from PyQt5.QtCore import *
+	print("this app use pyqt5")
+else:
+	from PySide2.QtCore import *
+	print("this app use pyside2")
  
 # class to handle button controls
 class Setting(QObject):
  
     # staGreen ON/OFF
-    @Slot()
+    @pyqtSlot()
     def staGreenOn(self):   
         # turn ON  
         os.system("sudo sh -c 'echo 255 > /sys/class/leds/usr_led2/brightness'")
-    @Slot()
+    @pyqtSlot()
     def staGreenOff(self):     
         # turn OFF  
         os.system("sudo sh -c 'echo 0 > /sys/class/leds/usr_led2/brightness'")
  
     # staRed
-    @Slot()
+    @pyqtSlot()
     def staRedOn(self):     
         os.system("sudo sh -c 'echo 255 > /sys/class/leds/usr_led1/brightness'")
-    @Slot()
+    @pyqtSlot()
     def staRedOff(self):     
         os.system("sudo sh -c 'echo 0 > /sys/class/leds/usr_led1/brightness'")
  
     # usrGreen
-    @Slot()
+    @pyqtSlot()
     def usrGreenOn(self):     
         os.system("sudo sh -c 'echo 255 > /sys/class/leds/usr_led0/brightness'")
-    @Slot()
+    @pyqtSlot()
     def usrGreenOff(self):     
         os.system("sudo sh -c 'echo 0 > /sys/class/leds/usr_led0/brightness'")
  
     # close
-    @Slot()
+    @pyqtSlot()
     def closeWindow(self):
         sys.exit()
--- LED_APP/led_start.sh
+++ LED_APP/led_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd $HOME/GUI/LED_APP
+cd /home/root/GUI_Demo_Custom
 DISPLAY=:0 python3 main.py
