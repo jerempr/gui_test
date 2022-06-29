82f4729954ef0b81e2fce601e424955bd70f4fdb
Jérémie Probst <jprobst@operametrix.fr>
2022-06-29 13:57:32 +0000

    changes harmonization of modbus.py

--- Debian_LED_APP/ModBus_Communication.py
+++ Debian_LED_APP/ModBus_Communication.py
@@ -151,12 +151,12 @@ class OperaMetrix_ModbusTCP_client():
 #     Read_addr(83)
 #     sleep(2)
 
-Myclient = OperaMetrix_ModbusTCP_client()
-Myclient.connect()
-testa = 101
-Myclient.Read_addr(testa)
-sleep(1)
-Myclient.Write_addr(testa,2.5)
-sleep(1)
-Myclient.Read_addr(testa)
-Myclient.close()
+# Myclient = OperaMetrix_ModbusTCP_client()
+# Myclient.connect()
+# testa = 101
+# Myclient.Read_addr(testa)
+# sleep(1)
+# Myclient.Write_addr(testa,2.5)
+# sleep(1)
+# Myclient.Read_addr(testa)
+# Myclient.close()
